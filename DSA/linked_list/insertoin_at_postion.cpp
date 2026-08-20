8b6f9daddfb0dd85527ee111fb69793c24f49507
diyapmr007rgb <diyapmr007@gmail.com>
2026-08-20 23:22:01 +0530

    Insertion at position

---
+++ DSA/linked_list/insertoin_at_postion.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+using namespace std;
+class Node
+{
+    public:
+    int data;
+    Node* next;
+
+};
+
+void insertAtPosition(Node* &head,int value,int position)
+{
+    Node* newnode=new Node();
+    newnode->data=value;
+
+    Node* temp=head;
+    for(int i=1;i<position-1;i++)
+    {
+        temp=temp->next;
+
+    }
+    newnode->next=temp->next;
+    temp->next=newnode;
+
+
+}
+
+int main()
+{
+    Node* node1=new Node();
+    Node* node2=new Node();
+    Node* node3=new Node();
+
+   cout<<"Enter node1:";
+   cin>>node1->data;
+   cout<<"Enter node2:";
+   cin>>node2->data;
+   cout<<"Enter node 3:";
+   cin>>node3->data;
+
+   node1->next=node2;
+   node2->next=node3;
+   node3->next=NULL;
+
+    Node* head=node1;
+    Node* temp=head;
+    cout<<"Original linked list:/n";
+
+       while(temp!=NULL)
+       {
+        cout<<temp->data<<"  ";
+        temp=temp->next;
+
+       }
+       int position,value;
+
+       cout<<"Enter the position :";
+       cin>>position;
+
+       cout<<"Enter the value to be add:";
+       cin>>value;
+
+       insertAtPosition(head,value,position);
+       cout<<"After insertoin:";
+       
+
+       temp=head;
+       while(temp!=NULL)
+       {
+        cout<<temp->data<<"  ";
+        temp=temp->next;
+
+       }
+       return 0;
+}
