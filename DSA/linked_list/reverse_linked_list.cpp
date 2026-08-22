531c7c4b834926c1b2f35802b91b36c8dfc7c3ad
diyapmr007rgb <diyapmr007@gmail.com>
2026-08-23 01:26:54 +0530

    reversed linked list

---
+++ DSA/linked_list/reverse_linked_list.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+class Node
+{
+    public:
+    int data;
+    Node* next;
+
+};
+
+void reversedlist(Node* &head)
+{
+    Node* prev=NULL;
+    Node* current=head;
+    Node* next=NULL;
+
+    while(current!=NULL)
+    {
+        next=current->next;
+        current->next=prev;
+        prev=current;
+        current=next;
+
+    }
+    head=prev;
+}
+
+int main()
+{
+    Node* node1=new Node();
+    Node* node2=new Node();
+    Node* node3=new Node();
+
+   cout<<"Enter node1:";
+   cin>>node1->data;
+   cout<<"Enter node2:";
+   cin>>node2->data;
+   cout<<"Enter node 3:";
+   cin>>node3->data;
+
+   node1->next=node2;
+   node2->next=node3;
+   node3->next=NULL;
+
+    Node* head=node1;
+    Node* temp=head;
+cout<<"Original list:";
+       while(temp!=NULL)
+       {
+        cout<<temp->data<<"  ";
+        temp=temp->next;
+
+       }
+
+       reversedlist(head);
+       temp=head;
+
+       
+       cout<<"After reversed:";
+        while(temp!=NULL)
+       {
+        cout<<temp->data<<"  ";
+        temp=temp->next;
+
+       }
+
+       return 0;
+}
